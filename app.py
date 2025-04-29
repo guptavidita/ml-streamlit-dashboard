27c19516cfeef725cfe207e3012e040476a9b398
guptavidita <136084610+guptavidita@users.noreply.github.com>
2025-04-29 17:58:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="ML Dashboard", layout="wide")
+
+st.title("🔍 Machine Learning Workflow Dashboard")
+st.markdown("Use the sidebar to navigate through the steps.")
